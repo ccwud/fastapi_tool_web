543e010a4ae32417a48d1bfd3545dd3a27d850cd
cc <562098412@qq.com>
2025-07-24 21:49:49 +0800

    111

--- tool-suite/vite.config.js
+++ tool-suite/vite.config.js
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools' // 暂时注释掉，因为与vite 7版本不兼容
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(), // 暂时注释掉
   ],
   resolve: {
     alias: {
